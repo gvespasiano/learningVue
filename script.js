18eb837334d7f64c6a63f5ec7d2dd8a992dd2b38
gvespasiano <gvespasiano@gmail.com>
2019-03-31 11:22:50 -0500

    Vue / HTML Structure

---
+++ script.js
@@ -0,0 +1,16 @@
+new Vue({
+  el: "#myApp",
+  data: {
+    title: "10 Llama Facts",
+	fact1:"1",
+	fact2:"1",
+	fact3:"1",
+	fact4:"1",
+	fact5:"1",
+	fact6:"1",
+	fact7:"1",
+	fact8:"1",
+	fact9:"1",
+    viewCheck: false,
+  }
+});
